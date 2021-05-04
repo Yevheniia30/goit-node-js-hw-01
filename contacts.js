09701b68cb4c45376bc9a4a1996a6b59eb6fd08b
Yevheniia Suska <susskaya86@gmail.com>
2021-05-05 02:21:36 +0300

    step 1 done

---
+++ contacts.js
@@ -0,0 +1,22 @@
+// не работает import
+
+// import path from 'path'
+// import fs from 'fs'
+const path=require('path')
+const fs = require('fs')
+
+// examples
+
+// fs.mkdir(path.join(__dirname, 'test-02'), (err) => {
+//     if (err) {
+//         throw err
+//     }
+
+//     console.log('folder created');
+// })
+
+
+
+// console.log(path.extname('index.html'));
+
+
