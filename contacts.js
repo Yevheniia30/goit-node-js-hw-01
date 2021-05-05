9cb6b86e63bbf21a2cb37b2bca5e8311d0d4ccf6
Yevheniia Suska <susskaya86@gmail.com>
2021-05-06 00:51:04 +0300

    step 3 done

--- contacts.js
+++ contacts.js
@@ -1,9 +1,33 @@
 // не работает import
 
+// Сделай импорт модулей fs и path для работы с файловой системой
 // import path from 'path'
 // import fs from 'fs'
 const path=require('path')
 const fs = require('fs')
+
+console.log('hi contacts');
+
+// Создай переменную contactsPath и запиши в нее путь к файле contacts.json. Для составления пути ипользуй методы модуля path.
+const contactsPath = ''
+
+// TODO: задокументировать каждую функцию
+function listContacts() {
+  // ...твой код
+}
+
+function getContactById(contactId) {
+  // ...твой код
+}
+
+function removeContact(contactId) {
+  // ...твой код
+}
+
+function addContact(name, email, phone) {
+  // ...твой код
+}
+
 
 // examples
 
--- index.js
+++ index.js
@@ -1,1 +1,3 @@
 console.log('Hello NodeJS');
+
+const contacts =require('./contacts')
